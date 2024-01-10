13d44f17b4df5ba4fcd27cca3bf3b178c796d627
Yash Tiwari <yash22012002@gmail.com>
2024-01-10 13:50:29 +0530

    stripe

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -35,7 +35,7 @@ const populateEvent = (query: any) => {
 export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
     await connectToDatabase();
-
+    console.log(userId);
     const organizer = await User.findById(userId);
     if (!organizer) throw new Error("Organizer not found");
 
